219e4562cb6d3d794087fb46d383470af918a4fa
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-06-21 11:22:42 -0400

    Initial Commit

---
+++ PositionEyesVision/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PositionEyesVision
+//
+//  Created by Caroline LaDouce on 6/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PositionEyesVision/PositionEyesVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PositionEyesVisionApp.swift
+//  PositionEyesVision
+//
+//  Created by Caroline LaDouce on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct PositionEyesVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
